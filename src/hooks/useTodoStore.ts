59487712b33e9be024ee8d5d48b319f47877b227
HYEJIN3 <88080040+HYEJIN3@users.noreply.github.com>
2024-12-16 18:21:47 +0900

    ✨ Add useTodoStore

--- src/hooks/useTodoStore.ts
+++ src/hooks/useTodoStore.ts
@@ -1,10 +1,16 @@
-f"use client";
+"use client";
 
 import { create } from "zustand";
 import { Todo, TodoFilter } from "../types/todo";
 
 interface TodoStore {
-  // to do
+  todos: Todo[];
+  filter: TodoFilter;
+  todoId: number;
+  addTodo: (text: string) => void;
+  toggleTodo: (id: number) => void;
+  deleteTodo: (id: number) => void;
+  setFilter: (filter: TodoFilter) => void;
 }
 
 export const useTodoStore = create<TodoStore>((set) => ({
@@ -13,15 +19,22 @@ export const useTodoStore = create<TodoStore>((set) => ({
   todoId: 0,
   addTodo: (text) =>
     set((state) => {
-      // to do
+      if (state.todos.filter((todo) => !todo.completed).length >= 10) {
+        alert("처리되지 않은 할 일은 10개를 넘을 수 없습니다.");
+        return state;
+      }
+      const newTodo: Todo = { id: state.todoId, text, completed: false };
+      return { todos: [...state.todos, newTodo], todoId: state.todoId + 1 };
     }),
   toggleTodo: (id) =>
     set((state) => ({
-      // to do
+      todos: state.todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      ),
     })),
   deleteTodo: (id) =>
     set((state) => ({
-      // to do
+      todos: state.todos.filter((todo) => todo.id !== id),
     })),
   setFilter: (filter) => set({ filter }),
 }));
